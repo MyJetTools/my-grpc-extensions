f8f73d340fd9057238d1f39a99fb885cb25c21d9
amigin <amigin@gmail.com>
2022-10-22 13:46:47 +0300

    Added flush_signle_item_to_stream

--- src/grpc_server.rs
+++ src/grpc_server.rs
@@ -1,6 +1,7 @@
 use std::{fmt::Debug, pin::Pin, time::Duration};
 
 use tokio::sync::mpsc::error::SendTimeoutError;
+const DEFAULT_SEND_TIMEOUT: Duration = Duration::from_secs(30);
 
 pub async fn create_empty_stream<TDest>() -> Result<
     tonic::Response<
@@ -19,6 +20,54 @@ where
     TDest: Send + Sync + Debug + 'static,
 {
     let (_tx, rx) = tokio::sync::mpsc::channel(1);
+
+    let output_stream = tokio_stream::wrappers::ReceiverStream::new(rx);
+    let response: Pin<
+        Box<dyn futures::Stream<Item = Result<TDest, tonic::Status>> + Send + Sync + 'static>,
+    > = Box::pin(output_stream);
+    return Ok(tonic::Response::new(response));
+}
+
+pub async fn flush_signle_item_to_stream<TDest, TFn>(
+    item: TDest,
+    #[cfg(feature = "adjust-server-stream")] send_timeout: Duration,
+) -> Result<
+    tonic::Response<
+        Pin<
+            Box<
+                dyn tonic::codegen::futures_core::Stream<Item = Result<TDest, tonic::Status>>
+                    + Send
+                    + Sync
+                    + 'static,
+            >,
+        >,
+    >,
+    tonic::Status,
+>
+where
+    TDest: Send + Sync + Debug + 'static,
+{
+    let (tx, rx) = tokio::sync::mpsc::channel(1);
+
+    tokio::spawn(async move {
+        #[cfg(not(feature = "adjust-server-stream"))]
+        let send_timeout = DEFAULT_SEND_TIMEOUT;
+
+        let sent_result = tx
+            .send_timeout(Result::<_, tonic::Status>::Ok(item), send_timeout)
+            .await;
+
+        if let Err(err) = sent_result {
+            match err {
+                SendTimeoutError::Timeout(err) => {
+                    println!("Can not send to grpc channel. Timeout. Err: {:?}", err);
+                }
+                SendTimeoutError::Closed(err) => {
+                    println!("Can not send to grpc channel. Its closed. Err: {:?}", err);
+                }
+            }
+        }
+    });
 
     let output_stream = tokio_stream::wrappers::ReceiverStream::new(rx);
     let response: Pin<
@@ -57,7 +106,7 @@ where
 
     tokio::spawn(async move {
         #[cfg(not(feature = "adjust-server-stream"))]
-        let send_timeout = Duration::from_secs(30);
+        let send_timeout = DEFAULT_SEND_TIMEOUT;
 
         for itm in src {
             let contract = mapping(itm);
@@ -118,7 +167,7 @@ where
 
     tokio::spawn(async move {
         #[cfg(not(feature = "adjust-server-stream"))]
-        let send_timeout = Duration::from_secs(30);
+        let send_timeout = DEFAULT_SEND_TIMEOUT;
         for (key, value) in src {
             let contract = mapping(key, value);
 
