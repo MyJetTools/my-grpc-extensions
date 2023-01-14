dde92c0d36406f30397ff3c4aef5045d56c7a5db
amigin <amigin@gmail.com>
2023-01-14 20:31:14 +0200

    Retries presented

--- src/grpc_channel.rs
+++ src/grpc_channel.rs
@@ -1,8 +1,15 @@
 use std::{collections::HashMap, time::Duration};
 
 use futures::Future;
-use tokio::sync::RwLock;
+use tokio::{sync::RwLock, time::error::Elapsed};
 use tonic::transport::Channel;
+
+#[derive(Debug)]
+pub enum GrpcReadError {
+    Timeout,
+    TransportError(tonic::transport::Error),
+    TonicStatus(tonic::Status),
+}
 
 pub struct GrpcChannel {
     pub channel: RwLock<Option<Channel>>,
@@ -19,43 +26,76 @@ impl GrpcChannel {
         }
     }
 
-    pub async fn get_channel(&self) -> Channel {
+    pub async fn get_channel(&self) -> Result<Channel, GrpcReadError> {
         {
             let access = self.channel.read().await;
             if let Some(channel) = access.as_ref() {
-                return channel.clone();
+                return Ok(channel.clone());
             }
         }
 
         let mut access = self.channel.write().await;
+
         if let Some(channel) = access.as_ref() {
-            return channel.clone();
+            return Ok(channel.clone());
         }
 
         let end_point = Channel::from_shared(self.grpc_address.clone()).unwrap();
 
-        let channel = tokio::time::timeout(self.timeout, end_point.connect())
-            .await
-            .unwrap()
-            .unwrap();
+        let channel = tokio::time::timeout(self.timeout, end_point.connect()).await?;
 
+        let channel = channel?;
         *access = Some(channel.clone());
 
-        channel
+        Ok(channel)
     }
 
-    pub async fn execute_with_timeout<TResult, TFuture: Future<Output = TResult>>(
+    pub async fn execute_with_timeout<
+        TResult,
+        TFuture: Future<Output = Result<TResult, tonic::Status>>,
+    >(
         &self,
-
         future: TFuture,
-    ) -> TResult {
+    ) -> Result<TResult, GrpcReadError> {
         let result = tokio::time::timeout(self.timeout, future).await;
+
+        if result.is_err() {
+            let mut access = self.channel.write().await;
+            *access = None;
+            return Err(GrpcReadError::Timeout);
+        }
+
+        let result = result.unwrap();
+
         match result {
-            Ok(result) => return result,
-            Err(_) => {
-                let mut access = self.channel.write().await;
-                *access = None;
-                panic!("Grpc {} is Timeouted", self.grpc_address);
+            Ok(result) => Ok(result),
+            Err(err) => {
+                let err = GrpcReadError::TonicStatus(err);
+                self.remove_channel_if_needed(&err).await;
+                Err(err)
+            }
+        }
+    }
+
+    pub async fn execute_with_retries<
+        TResult,
+        TFutureResult: Future<Output = Result<TResult, tonic::Status>>,
+        TGetFuture: Fn() -> TFutureResult,
+    >(
+        &self,
+        get_future: TGetFuture,
+        max_attempts_amount: usize,
+    ) -> Result<TResult, GrpcReadError> {
+        let mut attempt_no = 0;
+        loop {
+            let future = get_future();
+
+            match self.execute_with_timeout(future).await {
+                Ok(result) => return Ok(result),
+                Err(err) => {
+                    self.handle_timeout_error(err, &mut attempt_no, max_attempts_amount)
+                        .await?;
+                }
             }
         }
     }
@@ -66,12 +106,53 @@ impl GrpcChannel {
     >(
         &self,
         future: TFuture,
-    ) -> Option<Vec<TResult>> {
-        let response = self.execute_with_timeout(future).await.unwrap();
+    ) -> Result<Option<Vec<TResult>>, GrpcReadError> {
+        let response = self.execute_with_timeout(future).await?;
 
-        crate::read_grpc_stream::as_vec(response.into_inner(), self.timeout)
-            .await
-            .unwrap()
+        let result = crate::read_grpc_stream::as_vec(response.into_inner(), self.timeout).await;
+
+        match result {
+            Ok(result) => Ok(result),
+            Err(err) => {
+                self.remove_channel_if_needed(&err).await;
+                Err(err)
+            }
+        }
+    }
+
+    pub async fn execute_stream_as_vec_with_retries<
+        TResult,
+        TFuture: Future<Output = Result<tonic::Response<tonic::Streaming<TResult>>, tonic::Status>>,
+        TGetFuture: Fn() -> TFuture,
+    >(
+        &self,
+        get_future: TGetFuture,
+        max_attempts_amount: usize,
+    ) -> Result<Option<Vec<TResult>>, GrpcReadError> {
+        let mut attempt_no = 0;
+        loop {
+            let future = get_future();
+            let response = self.execute_with_timeout(future).await;
+
+            match response {
+                Ok(response) => {
+                    let result =
+                        crate::read_grpc_stream::as_vec(response.into_inner(), self.timeout).await;
+
+                    match result {
+                        Ok(result) => return Ok(result),
+                        Err(err) => {
+                            self.handle_timeout_error(err, &mut attempt_no, max_attempts_amount)
+                                .await?;
+                        }
+                    }
+                }
+                Err(err) => {
+                    self.handle_timeout_error(err, &mut attempt_no, max_attempts_amount)
+                        .await?;
+                }
+            }
+        }
     }
 
     pub async fn excute_stream_as_hash_map<
@@ -84,14 +165,128 @@ impl GrpcChannel {
         &self,
         future: TFuture,
         get_key: TGetKey,
-    ) -> Option<HashMap<TKey, TValue>>
+    ) -> Result<Option<HashMap<TKey, TValue>>, GrpcReadError>
     where
         TKey: std::cmp::Eq + core::hash::Hash + Clone,
     {
-        let response = self.execute_with_timeout(future).await.unwrap();
+        let response = self.execute_with_timeout(future).await?;
 
-        crate::read_grpc_stream::as_hash_map(response.into_inner(), get_key, self.timeout)
-            .await
-            .unwrap()
+        let result =
+            crate::read_grpc_stream::as_hash_map(response.into_inner(), &get_key, self.timeout)
+                .await;
+
+        match result {
+            Ok(result) => Ok(result),
+            Err(err) => {
+                self.remove_channel_if_needed(&err).await;
+                Err(err)
+            }
+        }
+    }
+
+    pub async fn excute_stream_as_hash_map_with_retries<
+        TSrc,
+        TKey,
+        TValue,
+        TFuture: Future<Output = Result<tonic::Response<tonic::Streaming<TSrc>>, tonic::Status>>,
+        TGetFuture: Fn() -> TFuture,
+        TGetKey: Fn(TSrc) -> (TKey, TValue),
+    >(
+        &self,
+        get_future: TGetFuture,
+        max_attempts_amount: usize,
+        get_key: TGetKey,
+    ) -> Result<Option<HashMap<TKey, TValue>>, GrpcReadError>
+    where
+        TKey: std::cmp::Eq + core::hash::Hash + Clone,
+    {
+        let mut attempt_no = 0;
+        loop {
+            let future = get_future();
+            let response = self.execute_with_timeout(future).await;
+
+            match response {
+                Ok(response) => {
+                    let result = crate::read_grpc_stream::as_hash_map(
+                        response.into_inner(),
+                        &get_key,
+                        self.timeout,
+                    )
+                    .await;
+
+                    match result {
+                        Ok(result) => return Ok(result),
+                        Err(err) => {
+                            self.handle_timeout_error(err, &mut attempt_no, max_attempts_amount)
+                                .await?;
+                        }
+                    }
+                }
+                Err(err) => {
+                    self.handle_timeout_error(err, &mut attempt_no, max_attempts_amount)
+                        .await?;
+                }
+            }
+        }
+    }
+
+    async fn remove_channel_if_needed(&self, err: &GrpcReadError) -> bool {
+        let remove = match err {
+            GrpcReadError::TonicStatus(status) => {
+                let code = status.code();
+
+                if code == tonic::Code::Unknown {
+                    true
+                } else {
+                    false
+                }
+            }
+            _ => false,
+        };
+
+        if remove {
+            let mut access = self.channel.write().await;
+            *access = None;
+        }
+
+        remove
+    }
+
+    async fn handle_timeout_error(
+        &self,
+        err: GrpcReadError,
+        attempt_no: &mut usize,
+        max_attempts_amount: usize,
+    ) -> Result<(), GrpcReadError> {
+        let channel_removed = self.remove_channel_if_needed(&err).await;
+        if *attempt_no >= max_attempts_amount {
+            return Err(err);
+        }
+
+        *attempt_no += 1;
+
+        if channel_removed {
+            Ok(())
+        } else {
+            Err(err)
+        }
+    }
+}
+
+impl From<Elapsed> for GrpcReadError {
+    fn from(_: Elapsed) -> Self {
+        Self::Timeout
+    }
+}
+
+impl From<tonic::Status> for GrpcReadError {
+    fn from(value: tonic::Status) -> Self {
+        Self::TonicStatus(value)
+    }
+}
+
+impl From<tonic::transport::Error> for GrpcReadError {
+    fn from(value: tonic::transport::Error) -> Self {
+        Self::TransportError(value)
     }
 }
--- src/read_grpc_stream.rs
+++ src/read_grpc_stream.rs
@@ -3,31 +3,23 @@ use std::{collections::HashMap, time::Duration};
 use futures_util::StreamExt;
 use rust_extensions::lazy::{LazyHashMap, LazyVec};
 
-#[derive(Debug)]
-pub enum ReadStreamError {
-    Timeout,
-    TonicError(tonic::Status),
-}
+use crate::GrpcReadError;
 
 pub async fn as_vec<T>(
     mut stream_to_read: tonic::Streaming<T>,
     timeout: Duration,
-) -> Result<Option<Vec<T>>, ReadStreamError> {
+) -> Result<Option<Vec<T>>, GrpcReadError> {
     let mut result = LazyVec::new();
 
     loop {
-        let response = tokio::time::timeout(timeout, stream_to_read.next()).await;
+        let response = tokio::time::timeout(timeout, stream_to_read.next()).await?;
 
-        if response.is_err() {
-            return Err(ReadStreamError::Timeout);
-        }
-
-        match response.unwrap() {
+        match response {
             Some(item) => match item {
                 Ok(item) => {
                     result.add(item);
                 }
-                Err(err) => Err(ReadStreamError::TonicError(err))?,
+                Err(err) => Err(GrpcReadError::TonicStatus(err))?,
             },
             None => {
                 return Ok(result.get_result());
@@ -38,28 +30,24 @@ pub async fn as_vec<T>(
 
 pub async fn as_hash_map<TSrc, TKey, TValue, TGetKey: Fn(TSrc) -> (TKey, TValue)>(
     mut stream_to_read: tonic::Streaming<TSrc>,
-    get_key: TGetKey,
+    get_key: &TGetKey,
     timeout: Duration,
-) -> Result<Option<HashMap<TKey, TValue>>, ReadStreamError>
+) -> Result<Option<HashMap<TKey, TValue>>, GrpcReadError>
 where
     TKey: std::cmp::Eq + core::hash::Hash + Clone,
 {
     let mut result = LazyHashMap::new();
 
     loop {
-        let response = tokio::time::timeout(timeout, stream_to_read.next()).await;
+        let response = tokio::time::timeout(timeout, stream_to_read.next()).await?;
 
-        if response.is_err() {
-            return Err(ReadStreamError::Timeout);
-        }
-
-        match response.unwrap() {
+        match response {
             Some(item) => match item {
                 Ok(item) => {
                     let (key, value) = get_key(item);
                     result.insert(key, value);
                 }
-                Err(err) => Err(ReadStreamError::TonicError(err))?,
+                Err(err) => Err(GrpcReadError::TonicStatus(err))?,
             },
             None => {
                 return Ok(result.get_result());
@@ -72,23 +60,19 @@ pub async fn as_vec_with_transformation<T, TDest, TFn: Fn(T) -> TDest>(
     mut stream_to_read: tonic::Streaming<T>,
     timeout: Duration,
     transform: TFn,
-) -> Result<Option<Vec<TDest>>, ReadStreamError> {
+) -> Result<Option<Vec<TDest>>, GrpcReadError> {
     let mut result = LazyVec::new();
 
     loop {
-        let response = tokio::time::timeout(timeout, stream_to_read.next()).await;
+        let response = tokio::time::timeout(timeout, stream_to_read.next()).await?;
 
-        if response.is_err() {
-            return Err(ReadStreamError::Timeout);
-        }
-
-        match response.unwrap() {
+        match response {
             Some(item) => match item {
                 Ok(item) => {
                     let item = transform(item);
                     result.add(item);
                 }
-                Err(err) => Err(ReadStreamError::TonicError(err))?,
+                Err(err) => Err(GrpcReadError::TonicStatus(err))?,
             },
             None => {
                 return Ok(result.get_result());
@@ -100,19 +84,15 @@ pub async fn as_vec_with_transformation<T, TDest, TFn: Fn(T) -> TDest>(
 pub async fn first_or_none<T>(
     mut streaming: tonic::Streaming<T>,
     timeout: Duration,
-) -> Result<Option<T>, ReadStreamError> {
-    let response = tokio::time::timeout(timeout, streaming.next()).await;
+) -> Result<Option<T>, GrpcReadError> {
+    let response = tokio::time::timeout(timeout, streaming.next()).await?;
 
-    if response.is_err() {
-        return Err(ReadStreamError::Timeout);
-    }
-
-    match response.unwrap() {
+    match response {
         Some(item) => match item {
             Ok(item) => {
                 return Ok(Some(item));
             }
-            Err(err) => Err(ReadStreamError::TonicError(err))?,
+            Err(err) => Err(GrpcReadError::TonicStatus(err))?,
         },
         None => {
             return Ok(None);
