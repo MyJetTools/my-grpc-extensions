c221b893b212f514dc5d1f617d5464b1dfc16c7d
amigin <amigin@gmail.com>
2023-09-17 16:17:34 +0300

    Added generate_server_stream;

---
+++ my-grpc-server-macros/src/generate_server_stream/mod.rs
@@ -0,0 +1,2 @@
+mod generate_server_stream;
+pub use generate_server_stream::*;
--- my-grpc-server-macros/src/lib.rs
+++ my-grpc-server-macros/src/lib.rs
@@ -1,10 +1,19 @@
 use proc_macro::TokenStream;
 
+mod generate_server_stream;
 mod with_telemetry;
 
 #[proc_macro_attribute]
 pub fn with_telemetry(attr: TokenStream, item: TokenStream) -> TokenStream {
     match crate::with_telemetry::generate(attr, item) {
+        Ok(result) => result,
+        Err(err) => err.into_compile_error().into(),
+    }
+}
+
+#[proc_macro]
+pub fn generate_server_stream(item: TokenStream) -> TokenStream {
+    match crate::generate_server_stream::generate_server_stream(item) {
         Ok(result) => result,
         Err(err) => err.into_compile_error().into(),
     }
