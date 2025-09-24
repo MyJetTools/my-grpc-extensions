5f01c63280b48e6c686d766c80e9c467044efca2
amigin <amigin@gmail.com>
2025-09-24 14:49:52 +0300

    Improvements

--- my-grpc-extensions/src/grpc_server_streams.rs
+++ my-grpc-extensions/src/grpc_server_streams.rs
@@ -81,6 +81,7 @@ where
     return Ok(tonic::Response::new(response));
 }
 
+#[deprecated(note = "please use `send_from_iterator` instead")]
 pub async fn send_vec_to_stream<TSrc, TDest>(
     src: impl Iterator<Item = TSrc> + Send + Sync + 'static,
     mapping: impl Fn(TSrc) -> TDest + Send + Sync + 'static,
@@ -114,6 +115,65 @@ where
 
         for itm in src {
             let contract = mapping(itm);
+
+            let sent_result = tx
+                .send_timeout(Result::<_, tonic::Status>::Ok(contract), send_timeout)
+                .await;
+
+            if let Err(err) = sent_result {
+                match err {
+                    SendTimeoutError::Timeout(err) => {
+                        println!("Can not send to grpc channel. Timeout. Err: {:?}", err);
+                        break;
+                    }
+                    SendTimeoutError::Closed(err) => {
+                        println!("Can not send to grpc channel. Its closed. Err: {:?}", err);
+                        break;
+                    }
+                }
+            }
+        }
+    });
+
+    let output_stream = tokio_stream::wrappers::ReceiverStream::new(rx);
+    let response: Pin<
+        Box<dyn futures::Stream<Item = Result<TDest, tonic::Status>> + Send + Sync + 'static>,
+    > = Box::pin(output_stream);
+    return Ok(tonic::Response::new(response));
+}
+
+pub async fn send_from_iterator<TSrc, TDest: From<TSrc>>(
+    src: impl Iterator<Item = TSrc> + Send + Sync + 'static,
+    #[cfg(feature = "adjust-server-stream")] channel_size: usize,
+    #[cfg(feature = "adjust-server-stream")] send_timeout: Duration,
+) -> Result<
+    tonic::Response<
+        Pin<
+            Box<
+                dyn futures_util::Stream<Item = Result<TDest, tonic::Status>>
+                    + Send
+                    + Sync
+                    + 'static,
+            >,
+        >,
+    >,
+    tonic::Status,
+>
+where
+    TSrc: Send + Sync + Debug + 'static,
+    TDest: From<TSrc> + Send + Sync + Debug + 'static,
+{
+    #[cfg(not(feature = "adjust-server-stream"))]
+    let channel_size = 100;
+
+    let (tx, rx) = tokio::sync::mpsc::channel(channel_size);
+
+    tokio::spawn(async move {
+        #[cfg(not(feature = "adjust-server-stream"))]
+        let send_timeout = DEFAULT_SEND_TIMEOUT;
+
+        for itm in src {
+            let contract: TDest = itm.into();
 
             let sent_result = tx
                 .send_timeout(Result::<_, tonic::Status>::Ok(contract), send_timeout)
