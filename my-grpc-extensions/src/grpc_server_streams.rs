286a69ebc6ac2026d1313a45d86db6a60aedb7a7
amigin <amigin@gmail.com>
2026-01-25 13:31:38 +0200

    Minor Improvements

--- my-grpc-extensions/src/grpc_server_streams.rs
+++ my-grpc-extensions/src/grpc_server_streams.rs
@@ -1,7 +1,6 @@
 use std::{fmt::Debug, pin::Pin, time::Duration};
 
 use tokio::sync::mpsc::error::SendTimeoutError;
-#[cfg(not(feature = "adjust-server-stream"))]
 const DEFAULT_SEND_TIMEOUT: Duration = Duration::from_secs(30);
 
 pub type SendStream<TDest> = tonic::Response<
@@ -35,7 +34,6 @@ where
 
 pub async fn send_single_item_to_stream<TDest>(
     item: TDest,
-    #[cfg(feature = "adjust-server-stream")] send_timeout: Duration,
 ) -> Result<
     tonic::Response<
         Pin<
@@ -55,7 +53,6 @@ where
     let (tx, rx) = tokio::sync::mpsc::channel(1);
 
     tokio::spawn(async move {
-        #[cfg(not(feature = "adjust-server-stream"))]
         let send_timeout = DEFAULT_SEND_TIMEOUT;
 
         let sent_result = tx
@@ -81,12 +78,12 @@ where
     return Ok(tonic::Response::new(response));
 }
 
-#[deprecated(note = "please use `send_from_iterator` instead")]
+#[deprecated(
+    note = "please use `send_from_iterator` or `send_from_iterator_with_transformation` instead"
+)]
 pub async fn send_vec_to_stream<TSrc, TDest>(
     src: impl Iterator<Item = TSrc> + Send + Sync + 'static,
     mapping: impl Fn(TSrc) -> TDest + Send + Sync + 'static,
-    #[cfg(feature = "adjust-server-stream")] channel_size: usize,
-    #[cfg(feature = "adjust-server-stream")] send_timeout: Duration,
 ) -> Result<
     tonic::Response<
         Pin<
@@ -104,13 +101,34 @@ where
     TSrc: Send + Sync + Debug + 'static,
     TDest: Send + Sync + Debug + 'static,
 {
-    #[cfg(not(feature = "adjust-server-stream"))]
+    send_from_iterator_with_transformation(src, mapping).await
+}
+
+pub async fn send_from_iterator_with_transformation<TSrc, TDest>(
+    src: impl Iterator<Item = TSrc> + Send + Sync + 'static,
+    mapping: impl Fn(TSrc) -> TDest + Send + Sync + 'static,
+) -> Result<
+    tonic::Response<
+        Pin<
+            Box<
+                dyn futures_util::Stream<Item = Result<TDest, tonic::Status>>
+                    + Send
+                    + Sync
+                    + 'static,
+            >,
+        >,
+    >,
+    tonic::Status,
+>
+where
+    TSrc: Send + Sync + Debug + 'static,
+    TDest: Send + Sync + Debug + 'static,
+{
     let channel_size = 100;
 
     let (tx, rx) = tokio::sync::mpsc::channel(channel_size);
 
     tokio::spawn(async move {
-        #[cfg(not(feature = "adjust-server-stream"))]
         let send_timeout = DEFAULT_SEND_TIMEOUT;
 
         for itm in src {
@@ -144,8 +162,6 @@ where
 
 pub async fn send_from_iterator<TSrc, TDest: From<TSrc>>(
     src: impl Iterator<Item = TSrc> + Send + Sync + 'static,
-    #[cfg(feature = "adjust-server-stream")] channel_size: usize,
-    #[cfg(feature = "adjust-server-stream")] send_timeout: Duration,
 ) -> Result<
     tonic::Response<
         Pin<
@@ -163,13 +179,11 @@ where
     TSrc: Send + Sync + Debug + 'static,
     TDest: From<TSrc> + Send + Sync + Debug + 'static,
 {
-    #[cfg(not(feature = "adjust-server-stream"))]
     let channel_size = 100;
 
     let (tx, rx) = tokio::sync::mpsc::channel(channel_size);
 
     tokio::spawn(async move {
-        #[cfg(not(feature = "adjust-server-stream"))]
         let send_timeout = DEFAULT_SEND_TIMEOUT;
 
         for itm in src {
@@ -206,8 +220,6 @@ pub async fn send_vec_to_stream_by_chunks<TSrc, TDest, TDestChunk>(
     chunk_size: usize,
     mapping: impl Fn(TSrc) -> TDest + Send + Sync + 'static,
     transform_to_contract: impl Fn(Vec<TDest>) -> TDestChunk + Send + Sync + 'static,
-    #[cfg(feature = "adjust-server-stream")] channel_size: usize,
-    #[cfg(feature = "adjust-server-stream")] send_timeout: Duration,
 ) -> Result<
     tonic::Response<
         Pin<
@@ -226,13 +238,11 @@ where
     TDest: Send + Sync + Debug + 'static,
     TDestChunk: Send + Sync + Debug + 'static,
 {
-    #[cfg(not(feature = "adjust-server-stream"))]
     let channel_size = 100;
 
     let (tx, rx) = tokio::sync::mpsc::channel(channel_size);
 
     tokio::spawn(async move {
-        #[cfg(not(feature = "adjust-server-stream"))]
         let send_timeout = DEFAULT_SEND_TIMEOUT;
 
         let mut chunk: Vec<TDest> = Vec::with_capacity(chunk_size);
@@ -297,8 +307,6 @@ where
 pub async fn send_hash_map_to_stream<TKeySrc, TValueSrc, TDest>(
     src: std::collections::HashMap<TKeySrc, TValueSrc>,
     mapping: impl Fn(TKeySrc, TValueSrc) -> TDest + Send + Sync + 'static,
-    #[cfg(feature = "adjust-server-stream")] channel_size: usize,
-    #[cfg(feature = "adjust-server-stream")] send_timeout: Duration,
 ) -> Result<
     tonic::Response<
         Pin<
@@ -317,12 +325,10 @@ where
     TValueSrc: Send + Sync + 'static,
     TDest: Send + Sync + Debug + 'static,
 {
-    #[cfg(not(feature = "adjust-server-stream"))]
     let channel_size = 100;
     let (tx, rx) = tokio::sync::mpsc::channel(channel_size);
 
     tokio::spawn(async move {
-        #[cfg(not(feature = "adjust-server-stream"))]
         let send_timeout = DEFAULT_SEND_TIMEOUT;
         for (key, value) in src {
             let contract = mapping(key, value);
