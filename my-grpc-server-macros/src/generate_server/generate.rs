6a1bb34f09aa23a784a93a526488f286723214ba
amigin <amigin@gmail.com>
2026-02-05 21:44:17 +0200

    Fixes

--- my-grpc-server-macros/src/generate_server/generate.rs
+++ my-grpc-server-macros/src/generate_server/generate.rs
@@ -114,7 +114,7 @@ pub fn generate(input: proc_macro2::TokenStream) -> Result<proc_macro::TokenStre
 
             #stream_description
 
-            async fn #fn_name(&self, request:#input_param , #telemetry_param)->Result<#out_type, tonic::Status>{
+            async fn #fn_name(&self, request:#input_param )->Result<#out_type, tonic::Status>{
 
                 #telemetry_injection
 
